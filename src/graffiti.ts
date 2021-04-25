634dc7c635b9a3f863cdebaeb383e17fd084a0a6
wakaholi <tao6128@gmail.com>
2021-04-25 21:58:54 +0900

    ctrl+dでcanvasの表示が切り替えられるようにした

--- src/graffiti.ts
+++ src/graffiti.ts
@@ -1,5 +1,15 @@
-(function initialize() {
-  console.log("page-graffiti-extension起動〜");
+const initEventHandler = async (event: KeyboardEvent): Promise<void> => {
+  if (event.key !== "d" || !event.ctrlKey) {
+    return;
+  }
+
+  console.log(document.getElementsByClassName("pge-draw-area").length);
+
+  if(document.getElementsByClassName("pge-draw-area").length === 1) {
+    const drawArea = document.getElementsByClassName("pge-draw-area")[0];
+    drawArea.remove();
+    return;
+  }
 
   const drawElement = document.createElement("canvas");
   drawElement.classList.add('pge-draw-area');
@@ -8,4 +18,10 @@
   const cssTest = `position: absolute; top: 0; left: 0; right: 0; bottom: 0; height: ${height}px; width: ${width}px;`;
   drawElement.style.cssText = cssTest;
   document.body.appendChild(drawElement);
+}
+
+(function initialize() {
+  console.log("page-graffiti-extension起動〜");
+
+  document.addEventListener("keydown", initEventHandler);
 })();
